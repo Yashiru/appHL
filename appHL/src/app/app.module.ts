2ad093ea6b0f3011e296870bc6841992843753c5
leo <fasanol.info@gmail.com>
2017-04-01 19:39:13 +0200

    initial commit

---
+++ appHL/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { MainPage } from '../pages/mainPage/mainPage';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    MainPage,
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MainPage,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ appHL/src/pages/mainPage/mainPage.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by leo on 01/04/17.
+ */
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'main-page',
+  templateUrl: 'mainPage.html'
+})
+export class MainPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
